d8bc9bd0751cdea9e3d5dcec40f556cdc7fbcc4a
Garima <garima@webkorps.com>
2021-05-18 11:00:18 +0530

    prepare to deploy

--- my-app/src/components/customTabs.js
+++ my-app/src/components/customTabs.js
@@ -5,21 +5,30 @@ import { Movie} from './movieDetails'
 import { Genre} from './genre'
 import {useEffect, useState} from 'react';
 
-
+const Loader = () => (
+  <div class="divLoader">
+    <svg className="svgLoader" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+  <circle cx="50" cy="50" fill="none" stroke="#fed136" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+    <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+  </circle>
+</svg>
+  </div>
+);
 
 function CustomTabs() {
   const [key, setKey] = useState('movie');
-  const [TabValue, setTabValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setTabValue(newValue);
-  };
   const [movieDetail, setMovieDetail] = useState([])
+  const [loading, setLoading] =useState(true)
   useEffect(() => {
     fetch('/movieAPI/').then(res => {
       if(res.ok) {
         return res.json()
       }
-    }).then((key,value) => setMovieDetail(key,value))
+    }).then((key,value) => {
+      setMovieDetail(key,value) 
+      setLoading(false)
+      
+    })
   },[])
   
   return (
@@ -29,13 +38,9 @@ function CustomTabs() {
         activeKey={key}
         onSelect={(k) => setKey(k)}
       >
-        <Tab 
-          eventKey="movie" 
-          title="List Of Movies"
-          value={TabValue}
-          onChange={handleChange}
-        >
-          <Movie movieDetail={movieDetail}/>
+        <Tab eventKey="movie" title="List Of Movies">
+          {loading ? <Loader /> : <Movie movieDetail={movieDetail}/>}
+          
         </Tab>
         <Tab eventKey="Genre" title="Genre">
           <Genre movieDetail={movieDetail}/>
